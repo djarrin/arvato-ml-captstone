c64b076c9f897cd135c45c8d2db7d2c7dd422603
David Jarrin <dmjarrin@gmail.com>
2023-12-08 17:04:46 +0000

    saving progress

--- train.py
+++ train.py
@@ -11,7 +11,8 @@ def train():
     train_data = pd.concat([pd.read_csv(file) for file in all_files], ignore_index=True)
 
     # Create a TabularPredictor instance and train the model
-    predictor = TabularPredictor(label="RESPONSE", verbosity=4, sample_weight='RECORD_WEIGHT', weight_evaluation=True, eval_metric='log_loss').fit(
+    # predictor = TabularPredictor(label="RESPONSE", verbosity=4, sample_weight='RECORD_WEIGHT', weight_evaluation=True).fit(
+    predictor = TabularPredictor(label="RESPONSE", verbosity=4, eval_metric='log_loss').fit(
         train_data=train_data,
         time_limit=600,
         presets="best_quality"
@@ -19,7 +20,9 @@ def train():
 
     # Save the model to the output directory (S3 URI)
     output_path = '/opt/ml/model'  # SageMaker default output directory
-    predictor.save(output_path)
+    model_directory = os.path.join(output_path, 'model')
+
+    predictor.save(model_directory))
 
 if __name__ == '__main__':
     train()
